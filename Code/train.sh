7e03ef71967bb984b4d71c3e3a017856e17449d4
Skylar Wurster <swwurster@gmail.com>
2021-07-22 14:43:40 -0400

    Update train.sh

--- Code/train.sh
+++ Code/train.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd /lus/theta-fs0/projects/DL4VIS/Hierarchical_ACORN
 
-python3 Code/main.py --train_distributed true --save_name Cat_16GPUs_noresidual --log_img false
+python3 Code/train.py --train_distributed true --save_name Cat_16GPUs_noresidual --log_img false
