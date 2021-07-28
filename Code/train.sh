a49a275493cd0ef3151e2d568ce11a51d5d17cc0
Skylar Wurster <swwurster@gmail.com>
2021-07-28 09:30:34 -0400

    Update train.sh

--- Code/train.sh
+++ Code/train.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd /lus/theta-fs0/projects/DL4VIS/Hierarchical_ACORN
 
-python3 Code/train.py --save_name Cat_1GPU_residual --use_residual true --log_img false
+python3 Code/train.py --save_name Cat_16GPUs --use_residual false --log_img true --train_distributed true --gpus_per_node 8 --num_nodes 2
