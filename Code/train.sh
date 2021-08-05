96c6defe8d4fc25d9340b7e7626dda1fe4943259
Skylar Wurster <swwurster@gmail.com>
2021-08-04 21:31:16 -0400

    Update train.sh

--- Code/train.sh
+++ Code/train.sh
@@ -3,18 +3,22 @@
 
 #python3 Code/train.py --save_name Cat_16GPUs --use_residual false --log_img true --train_distributed true --gpus_per_node 8 --num_nodes 2
 
-python -u Code/train.py --use_residual true --FC_size_exp_start 1 --FC_size_exp_grow 0 \
---octree_depth_end 2 --save_name pluto_2models_size1 --target_signal TrainingData/pluto.h5 \
+python -u Code/train.py --use_residual true --FC_size_exp_start 2 --FC_size_exp_grow 0 \
+--octree_depth_end 4 --save_name pluto_4models_size2 --target_signal TrainingData/pluto.h5 \
 --num_channels 3 --mode 2D --local_queries_per_iter 1000000
 
-python -u Code/train.py --use_residual true --FC_size_exp_start 1 --FC_size_exp_grow 1 \
---octree_depth_end 2 --save_name pluto_2models_size2 --target_signal TrainingData/pluto.h5 \
+python -u Code/train.py --use_residual true --FC_size_exp_start 3 --FC_size_exp_grow 0 \
+--octree_depth_end 4 --save_name pluto_models_size3 --target_signal TrainingData/pluto.h5 \
 --num_channels 3 --mode 2D --local_queries_per_iter 1000000
 
-python -u Code/train.py --use_residual true --FC_size_exp_start 1 --FC_size_exp_grow 2 \
---octree_depth_end 2 --save_name pluto_2models_size3 --target_signal TrainingData/pluto.h5 \
+python -u Code/train.py --use_residual true --FC_size_exp_start 4 --FC_size_exp_grow 0 \
+--octree_depth_end 4 --save_name pluto_4models_size4 --target_signal TrainingData/pluto.h5 \
 --num_channels 3 --mode 2D --local_queries_per_iter 1000000
 
-python -u Code/train.py --use_residual true --FC_size_exp_start 1 --FC_size_exp_grow 3 \
---octree_depth_end 2 --save_name pluto_2models_size4 --target_signal TrainingData/pluto.h5 \
+python -u Code/train.py --use_residual true --FC_size_exp_start 5 --FC_size_exp_grow 0 \
+--octree_depth_end 4 --save_name pluto_4models_size5 --target_signal TrainingData/pluto.h5 \
+--num_channels 3 --mode 2D --local_queries_per_iter 1000000
+
+python -u Code/train.py --use_residual true --FC_size_exp_start 6 --FC_size_exp_grow 0 \
+--octree_depth_end 4 --save_name pluto_4models_size6 --target_signal TrainingData/pluto.h5 \
 --num_channels 3 --mode 2D --local_queries_per_iter 1000000
