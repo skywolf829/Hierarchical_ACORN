e4b88a2d7c98b7e6ea50ef368077bbb3828c6679
Skylar Wurster <swwurster@gmail.com>
2021-08-04 21:00:52 -0400

    Update train.sh

--- Code/train.sh
+++ Code/train.sh
@@ -4,17 +4,17 @@
 #python3 Code/train.py --save_name Cat_16GPUs --use_residual false --log_img true --train_distributed true --gpus_per_node 8 --num_nodes 2
 
 python -u Code/train.py --use_residual true --FC_size_exp_start 1 --FC_size_exp_grow 0 \
---octree_depth_end 2 --save_name Sinc_2models_fc1 --target_signal TrainingData/3D_toydata.h5 \
---num_channels 1 --mode 3D --error_bound 100
+--octree_depth_end 1 --save_name pluto_2models_size1 --target_signal TrainingData/pluto.h5 \
+--num_channels 3 --mode 2D --local_queries_per_iter 1000000
 
 python -u Code/train.py --use_residual true --FC_size_exp_start 2 --FC_size_exp_grow 0 \
---octree_depth_end 2 --save_name Sinc_2models_fc2 --target_signal TrainingData/3D_toydata.h5 \
---num_channels 1 --mode 3D --error_bound 100
+--octree_depth_end 2 --save_name pluto_2models_size2 --target_signal TrainingData/pluto.h5 \
+--num_channels 3 --mode 2D --local_queries_per_iter 1000000
+
+python -u Code/train.py --use_residual true --FC_size_exp_start 3 --FC_size_exp_grow 0 \
+--octree_depth_end 3 --save_name pluto_2models_size3 --target_signal TrainingData/pluto.h5 \
+--num_channels 3 --mode 2D --local_queries_per_iter 1000000
 
 python -u Code/train.py --use_residual true --FC_size_exp_start 4 --FC_size_exp_grow 0 \
---octree_depth_end 2 --save_name Sinc_2models_fc4 --target_signal TrainingData/3D_toydata.h5 \
---num_channels 1 --mode 3D --error_bound 100
-
-python -u Code/train.py --use_residual true --FC_size_exp_start 8 --FC_size_exp_grow 0 \
---octree_depth_end 2 --save_name Sinc_2models_fc8 --target_signal TrainingData/3D_toydata.h5 \
---num_channels 1 --mode 3D --error_bound 100
+--octree_depth_end 2 --save_name pluto_2models_size4 --target_signal TrainingData/pluto.h5 \
+--num_channels 3 --mode 2D --local_queries_per_iter 1000000
