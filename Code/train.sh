d52fc08d124407d9ed00cee2c827e5e9ffccc5a1
Skylar Wurster <swwurster@gmail.com>
2021-08-04 22:05:48 -0400

    Update train.sh

--- Code/train.sh
+++ Code/train.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-#cd /lus/theta-fs0/projects/DL4VIS/Hierarchical_ACORN
+!/bin/sh
+cd /lus/theta-fs0/projects/DL4VIS/Hierarchical_ACORN
 
 #python3 Code/train.py --save_name Cat_16GPUs --use_residual false --log_img true --train_distributed true --gpus_per_node 8 --num_nodes 2
 
