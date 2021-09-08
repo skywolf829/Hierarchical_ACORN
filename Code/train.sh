e0c4dbc66f800f7caa7de443e2b1fb6c553b3e03
Skylar Wurster <swwurster@gmail.com>
2021-09-08 11:31:19 -0400

    Update train.sh

--- Code/train.sh
+++ Code/train.sh
@@ -3,7 +3,7 @@
 
 #python3 Code/train.py --save_name Cat_16GPUs --use_residual false --log_img true --train_distributed true --gpus_per_node 8 --num_nodes 2
 
-python3 -u Code/train.py --use_residual false --FC_size_exp_start 1.0 --FC_size_exp_grow 1.2 \
+python3 -u Code/train.py --use_residual false --FC_size_exp_start 1.0 --FC_size_exp_grow 1.25 \
 --octree_depth_end 7 --save_name Pluto_8gpus --target_signal TrainingData/pluto.h5 \
 --num_channels 3 --mode 2D --local_queries_per_iter 1000000 --train_distributed true --num_nodes 1 \
 --gpus_per_node 8 --feat_grid_channels 16
